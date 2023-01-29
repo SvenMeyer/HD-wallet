92b59d1e2b8793b81417a58f0ba942d40d5abdce
Sven Meyer <git_dev@svenmeyer.com>
2023-01-29 21:35:50 +1100

    refactor: add simplified wallet version, add API-key

--- accounts-from-seed-hdnode.js
+++ accounts-from-seed-hdnode.js
@@ -29,6 +29,7 @@ async function main() {
     let pk;
     let balance = ethers.BigNumber.from(0);
     let balanceString;
+    let apiKeyAlchemy = "demo";
 
     if (process.argv.length >= 3) {
 	    mnemonic = process.argv[2];
@@ -42,6 +43,12 @@ async function main() {
     }
     console.log("accounts to generate =", n);
 
+    if (process.env.ALCHEMY_API_KEY.length == 32) {
+        apiKeyAlchemy = process.env.ALCHEMY_API_KEY;
+    }
+
+    console.log("API KEY = ", apiKeyAlchemy);
+
     if (process.argv.length >= 5) {
 	    networkish = process.argv[4];
         console.log("network name / chainId =", networkish);
@@ -52,30 +59,61 @@ async function main() {
             network = ethers.providers.getNetwork(parseInt(networkish));
         }
             
-        provider = new ethers.providers.AlchemyProvider(network, 'demo');
+        provider = new ethers.providers.AlchemyProvider(network, apiKeyAlchemy);
         console.log("network to get account balances from =", provider.network);
     }
 
-    wallet = ethers.Wallet.fromMnemonic(mnemonic);
-    console.log("  0", (derivePath + "  ").substring(0,18), "  0", wallet.address + ' <== to be expected for index 0');
+    path = derivePath; // ethers.utils.defaultPath; // "m/44'/60'/0'/0/0"
+    wallet = ethers.Wallet.fromMnemonic(mnemonic, path);
+    console.log("  0", (path + "  ").substring(0,18), "  ?", wallet.address + ' <== to be expected for index 0');
+
+    path = "m/44'/60'/0'/0/1"
+    wallet = ethers.Wallet.fromMnemonic(mnemonic, path);
+    console.log("  1", (path + "  ").substring(0,18), "  ?", wallet.address + ' <== to be expected for index 1 (MetaMask compatible)');
+
+    path = "m/44'/60'/1'/0/0"
+    wallet = ethers.Wallet.fromMnemonic(mnemonic, path);
+    console.log("  1", (path + "  ").substring(0,18), "  ?", wallet.address + ' <== to be expected for index 1');
+    
+    console.log();
+
+    // HDNode version
 
     let hdnodeParent = ethers.utils.HDNode.fromMnemonic(mnemonic);
 
     for (i=0; i<n; i++) {
         path = derivePath.slice(0,-1) + i;
         hdnode = hdnodeParent.derivePath( path );
-        index = hdnode.index;
-        address = hdnode.publicKey;
+        index =  hdnode.index;
+        // address = hdnode.publicKey;
         pk = hdnode.privateKey;
         wallet = new ethers.Wallet(pk);
+    //  wallet = ethers.Wallet.fromMnemonic(mnemonic, path); // simpler version (see below)
         if (networkish !== undefined) {
             balance = await provider.getBalance(wallet.address);
             balanceString = ethers.utils.formatUnits(balance) + " ETH";
         } else {
             balanceString = "";
         }
-        console.log(("  " + i).slice(-3), (path + "  ").substring(0,18), ("  " + index).slice(-3), wallet.address, pk, balanceString);
+        console.log(("  " + i).slice(-3), (path + "  ").substring(0,18), ("  " + index).slice(-3), wallet.address, wallet.privateKey, balanceString);
     }
+
+    // ether.wallet version
+
+    /*
+    for (i=0; i<n; i++) {
+        path = derivePath.slice(0,-1) + i;
+        wallet = ethers.Wallet.fromMnemonic(mnemonic, path);
+        if (networkish !== undefined) {
+            balance = await provider.getBalance(wallet.address);
+            balanceString = ethers.utils.formatUnits(balance) + " ETH";
+        } else {
+            balanceString = "";
+        }
+        console.log(("  " + i).slice(-3), (path + "  ").substring(0,18), wallet.address, wallet.privateKey, balanceString); // wallet.index is not available
+    }
+    */
+
 }
 
 main();
